3994c443ad6d99ef2d3dd57c9be03840e3b83c8a
Chris White <coldliquidflame@gmail.com>
2015-10-14 00:37:25 -0500

    Created factory for cordova plugins

--- www/app/app.js
+++ www/app/app.js
@@ -1,34 +1,65 @@
-(function(){
-	angular
-		.module('app', [])
-		.factory('cordova', cordovaFactory)
-		.controller('DefaultCtrl', defaultController);
-		
-	function cordovaFactory($document, $q) {	
-		var deferred = $q.defer();
-				
+(function() {
+  angular
+    .module('app', [])
+    .factory('cordova', cordovaFactory)
+		.factory('networkInfo', networkInfoFactory)
+    .controller('DefaultCtrl', defaultController);
+
+  function cordovaFactory($document, $q) {
+    var isDeviceReady = true;
+
+    var service = {
+      isReady: isReady
+    };
+
+    return service;
+
+
+
+    function isReady() {
+      var deferred = $q.defer();
+
+      if (isDeviceReady === true) {
+        deferred.resolve('true');
+      } else {
+        document.addEventListener('deviceready', deviceReadyCallback, false);
+
+        function deviceReadyCallback() {
+          deferred.resolve('true');
+        }
+      }
+
+      return deferred.promise;
+    }
+
+
+  }
+
+	function networkInfoFactory($window, $q, cordova) {
 		var service = {
-			isReady: isReady
-		}; 
-		
-		return service;		
-		
-		$document.addEventListener('deviceready', deviceReadyCallback, false);
-		
-		function isReady() {
+			connectionType: connectionType
+		}
+
+		return service;
+
+		function connectionType () {
+			var deferred = $q.defer();
+
+			cordova
+				.isReady()
+				.then(function() {
+					deferred.resolve(angular.toJson(navigator.connection.type));
+				});
+
 			return deferred.promise;
 		}
-		
-		function deviceReadyCallback() {
-			deferred.resolve(true);
-		}
 	}
-	
-	function defaultController($scope, cordova) {
-		$scope.message = "Cordova is not ready.";
-		
-		cordova.isReady().then(function(deviceReady) {
-			$scope.message = "Cordova is ready!";
-		});
-	}
+
+  function defaultController($scope, networkInfo) {
+    $scope.message = "Cordova is not ready.";
+
+    networkInfo.connectionType().then(function(connectionType) {
+      $scope.message = connectionType;
+    });
+  }
 })();
